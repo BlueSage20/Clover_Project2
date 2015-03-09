502ca0281491bf3ad6e37e800450924f189c0332
James Sasson <jms7396@g.rit.edu>
2015-03-09 19:09:05 -0400

    Add Card and Deck Scripts v1.0
    The Card class, for now, just has the public variables for name, type, attack, defense, effect1, and effect2.
    
    The Deck class will generate a new Deck on start and draw through the deck. This needs the CardList text file to work. When adding a deck to an object, drag and drop the CardList text file into the cardStuff (to be renamed later) global variable.

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Deck : MonoBehaviour {
+
+	public List<Card> cards = new List<Card>();
+	public TextAsset cardStuff;
+	private static int numCards;
+
+	// Use this for initialization
+	void Start () 
+	{
+		numCards = 30;
+		GenerateDeck ();
+		Card currentCard = Draw ();
+		while(currentCard != null)
+		{
+			Debug.Log(currentCard.cardName);
+			Debug.Log(currentCard.type);
+			currentCard = Draw ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+	}
+
+	public void Shuffle()
+	{
+
+	}
+
+	public Card Draw()
+	{
+		return cards.Remove;
+	}
+
+	private void GenerateDeck()
+	{
+		string[] cardInfo = ReadCards ();
+
+		for(int i = 0; i < numCards; i++)
+		{
+			string[] info = cardInfo[i].Split(',');
+			cards.Add(new Card(info[0], info[1], info[2], info[3], info[4], info[5], info[6], info[7]));
+		}
+	}
+
+	private string[] ReadCards()
+	{
+		string fileContents = cardStuff.text;
+		string [] cardContents = fileContents.Split ("\n"[0]);
+		for(int i = 0; i < numCards; i++)
+		{
+			Debug.Log(cardContents[i]);
+		}
+
+		return cardContents;
+	}
+}
